6250d00af8e897c932dcfecc6dbb959922469fd6
Claire Johnson <c.eliseee@gmail.com>
2017-11-29 17:00:21 -0800

    Refactor event listeners

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -4,26 +4,33 @@ class Drumkit {
   }
 
   addListener() {
-    window.addEventListener('keydown', this.handleKeydown.bind(this));
-    window.addEventListener('keyup', this.handleKeyup.bind(this));
+    window.addEventListener('keydown', (e) => {this.handleKey(e, true)});
+    window.addEventListener('keyup', (e) => {this.handleKey(e, false)});
   }
 
-  handleKeydown(e) {
+  handleKey(e, isKeydown) {
     const key = e.keyCode;
     const keyElements = document.querySelectorAll(`[data-key="${key}"]`);
     if (!keyElements.length) { return; }
+    if (isKeydown) {
+      this.handleKeydown(keyElements);
+    } else {
+      this.handleKeyup(keyElements);
+    }
+  }
+
+  handleKeydown(keyElements) {
     // Add highlight class to keyElements[0]
     keyElements[0].classList.add('playing');
-    // Play audio file of keyElements[1];
+    // Play audio file of keyElements[1]
+    // Allow for spam clicks: Pause and reset audio to 0 if already playing
     keyElements[1].pause();
+    keyElements[1].currentTime = 0;
     keyElements[1].play();
-
   }
 
-  handleKeyup(e) {
-    const key = e.keyCode;
-    const keyElements = document.querySelectorAll(`[data-key="${key}"]`);
-    if (!keyElements.length) { return; }
+  handleKeyup(keyElements) {
+    // Remove highlight class
     keyElements[0].classList.remove('playing');
   }
 }
