e4ec99922aaa47c1543f166d69c9361166742cb0
Claire Johnson <c.eliseee@gmail.com>
2017-12-02 13:22:58 -0800

    Finished clock

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -1,15 +1,35 @@
 class Clock {
   constructor() {
-    this.time = new Date();
-    this.renderClock();
-    setInterval(() = {
-      this.time = new Date();
-      this.renderClock();
+    this.setup();
+    setInterval(() => {
+      this.setup();
     }, 1000);
   }
 
+  setup() {
+    this.time = new Date();
+    this.updateTime();
+    this.renderClock();
+  }
+  
+  updateTime() {
+    this.hour = this.time.getHours();
+    this.min = this.time.getMinutes();
+    this.sec = this.time.getSeconds();
+  }
+
   renderClock() {
-    
+    const [hour, min, sec] = [
+      document.getElementById('hour'),
+      document.getElementById('min'),
+      document.getElementById('sec'),
+    ];
+    const hourDeg = 90 + (this.hour >= 13 ? (this.hour - 12) * (360 / 12) : this.hour * (360 / 12));
+    const minDeg = 90 + this.min * (360 / 60);
+    const secDeg = 90 + this.sec * (360 / 60);
+    hour.style.transform = `rotate(${hourDeg}deg)`;
+    min.style.transform = `rotate(${minDeg}deg)`;
+    sec.style.transform = `rotate(${secDeg}deg)`;
   }
 }
 
